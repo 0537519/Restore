8867a9d915558cfa12657c0cb2f6a260b0dc75a9
0537519 <1409822403@qq.com>
2025-02-08 12:47:23 +1100

    118

--- API/Data/StoreContext.cs
+++ API/Data/StoreContext.cs
@@ -1,15 +1,27 @@
 using System;
 using System.Net.Sockets;
 using API.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Data;
 
-public class StoreContext(DbContextOptions options) : DbContext(options)
+public class StoreContext(DbContextOptions options) : IdentityDbContext<User>(options)
 {
     public required DbSet<Product> Products { get; set; }
 
     public required DbSet<Basket> Baskets{ get; set; }
 
-   
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.Entity<IdentityRole>()
+           .HasData(
+            new IdentityRole{Id="af4a8244-b1d8-495f-8925-5a19f6d6229f", Name="Member",NormalizedName="MEMBER"},
+            new IdentityRole{Id="1a0f8489-3ed7-4099-9185-089d2b15d99c",Name="Admin",NormalizedName="ADMIN"}
+           );
+    }
+
+
 }
