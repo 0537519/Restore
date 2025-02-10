baa82844f35741f6c97ebd0668e714327a8988bb
0537519 <1409822403@qq.com>
2025-02-11 10:02:35 +1100

    135

---
+++ API/Controllers/PaymentsController.cs
@@ -0,0 +1,35 @@
+using System;
+using API.Data;
+using API.DTOs;
+using API.Extensions;
+using API.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+public class PaymentsController(PaymentsService paymentsService, StoreContext context):BaseApiController
+{
+    [Authorize]
+    [HttpPost]
+
+    public async Task<ActionResult<BasketDto>> CreateOrUpdatedPaymentIntent()
+    {
+        var basket=await context.Baskets.GetBasketWithItems(Request.Cookies["basketId"]);
+
+        if(basket==null) return BadRequest("Prob;em with the basket");
+
+        var intent =await paymentsService.CreateOrUpdatePaymentIntent(basket);
+
+        if(intent==null) return BadRequest("Problem creating payment intent");
+
+        basket.PaymentIntentId ??=intent.Id;
+        basket.ClientSecret ??=intent.ClientSecret;
+
+        var result =await context.SaveChangesAsync()>0;
+
+        if(!result) return BadRequest("Problem updating baskte with intent");
+
+        return basket.ToDto();
+    }
+}
